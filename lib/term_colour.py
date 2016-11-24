a8fe02afca0154ef08c0b6fd46899a93456c7a5e
Andy Driver <andy@pagezero.net>
2016-11-24 18:09:26 +0000

    Nicer console output on stopping the app

--- lib/term_colour.py
+++ lib/term_colour.py
@@ -1,18 +1,22 @@
-def in_colour(msg, fg, bg=None):
-    return '\x1b[0;{}{}m{}\x1b[0m'.format(fg, (';' + bg if bg else ''), msg)
+def in_colour(msg, bold=0, fg=None, bg=None):
+    return '\x1b[{}{}{}m{}\x1b[0m'.format(
+        bold,
+        (';{}'.format(fg) if fg else ''),
+        (';{}'.format(bg) if bg and fg else ''),
+        msg)
 
 
 def notify(msg):
-    print(in_colour(msg, 37))
+    print(in_colour(msg, bold=1))
 
 
 def status_ok(msg):
-    print(in_colour(msg, 32))
+    print(in_colour(msg, fg=32))
 
 
 def status_ok_info(msg):
-    print(in_colour(msg, 34))
+    print(in_colour(msg, fg=34))
 
 
 def status_err(msg):
-    print(in_colour(msg, 31))
+    print(in_colour(msg, fg=31))
