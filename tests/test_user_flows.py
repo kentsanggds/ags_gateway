05922d8cdc83393243fdb9c8ca6b31ecfa8a08d5
Josh Rowe <josh@s-block.com>
2016-12-20 16:15:12 +0000

    Add test for email addres with broker looup redirect

--- tests/test_user_flows.py
+++ tests/test_user_flows.py
@@ -25,6 +25,24 @@ class TestUserFlows(object):
             assert session['suggested_idp'] == idp
             assert session['department_name'] == name
 
+    @pytest.mark.parametrize("email", [
+        'test@digital.cabinetoffice.gov.uk',
+        'test.test@cabinet-office.gov.uk',
+        'test@sso.civilservice.digital',
+        'test@test.com',
+    ])
+    def test_confirm_email_address_broker_lookup_redirect(self, app_, email):
+        url = url_for('main.request_email_address')
+        data = {
+            'email_known': 'yes',
+            'email_address': email
+        }
+        with app_.test_client() as c:
+            resp = c.post(url, data=data)
+            assert session['idp_hint'] == 'idp of last resort'
+            assert resp.status_code == 302
+            assert resp.location.endswith(
+                url_for('broker.auth', idp_hint=session['idp_hint']))
     @pytest.mark.xfail
     def test_change_email_address(self):
         assert False
