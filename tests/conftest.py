70b560647204526486fe7318d091d6de3f7ddfe8
Josh Rowe <josh@s-block.com>
2016-12-21 15:35:01 +0000

    Mock oic.oic.Client.provider_config in tests so a request is not sent to the provider

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,17 @@
+import mock
 import pytest
 
 from app.factory import create_app
 
 
+def _provider_config(*args, **kwargs):
+    pass
+
+
 @pytest.fixture(scope='session')
 def app_(request):
-    app = create_app()
+    with mock.patch('oic.oic.Client.provider_config', _provider_config):
+        app = create_app()
     app.config['TESTING'] = True
     app.config['WTF_CSRF_ENABLED'] = False
 
