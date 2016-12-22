b33f3da86150136affdf8d5e8bbf5f9620eb9506
Josh Rowe <josh@s-block.com>
2016-12-22 14:44:08 +0000

    Add test for OICDClient sending authentication request

--- tests/test_oidc_client.py
+++ tests/test_oidc_client.py
@@ -1,11 +1,39 @@
+from flask import session, url_for
+import mock
 import pytest
 
 
 class TestOIDCClient(object):
 
-    @pytest.mark.xfail
-    def test_send_authentication_request(self):
-        assert False
+    @mock.patch('oic.oic.Client.construct_AuthorizationRequest')
+    def test_send_authentication_request(self, mock_auth_request, app_,
+                                         client):
+        callback_url = url_for('oidc_callback', _external=True)
+
+        with client.session_transaction() as sess:
+            sess['idp_hint'] = 'mock_idp_hint'
+            sess['email_address'] = 'mock_email_address'
+
+        resp = client.get(url_for('broker.auth'))
+
+        mock_auth_request.assert_called_once_with(request_args={
+            'client_id': app_.config['OIDC_CLIENT']['client_id'],
+            'response_type': 'code',
+            'scope': ['openid', 'profile'],
+            'redirect_uri': callback_url,
+            'state': session['state'],
+            'nonce': session['nonce'],
+            'kc_idp_hint': 'mock_idp_hint',
+            'login_hint': 'mock_email_address',
+            'claims': {
+                'userinfo': {
+                    'email': {'essential': True},
+                    'name': {'essential': True}
+                }
+            }
+        })
+
+        assert resp.status_code == 302
 
     @pytest.mark.xfail
     def test_send_token_request(self):
