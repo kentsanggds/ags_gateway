cea55338c5dabb9673d28326c5b9456d05910927
Josh Rowe <josh@s-block.com>
2016-12-23 15:40:07 +0000

    Add test for provider token request

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,3 +29,42 @@ def app_(request):
 def client(app_):
     with app_.test_client() as c:
         yield c
+
+
+@pytest.yield_fixture
+def request_params():
+    return {
+        'grant_type': 'authorization_code',
+        'code': 'SplxlOBeZQQYbYS6WxSbIA',
+        'redirect_uri': '/mock_url',
+    }
+
+
+@pytest.yield_fixture
+def request_headers(app_):
+    return {
+        'Content-Length': '0',
+        'Host': app_.config['SERVER_NAME'],
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic czZCaGRSa3F0MzpnWDFmQmF0M2JW',
+    }
+
+
+@pytest.yield_fixture
+def token_response():
+    return {
+        'access_token': 'SlAV32hkKG',
+        'token_type': 'Bearer',
+        'refresh_token': '8xLOxBtZp8',
+        'expires_in': 3600,
+        'id_token': '''eyJhbGciOiJSUzI1NiIsImtpZCI6IjFlOWdkazcifQ.ewogImlzc
+    yI6ICJodHRwOi8vc2VydmVyLmV4YW1wbGUuY29tIiwKICJzdWIiOiAiMjQ4Mjg5
+    NzYxMDAxIiwKICJhdWQiOiAiczZCaGRSa3F0MyIsCiAibm9uY2UiOiAibi0wUzZ
+    fV3pBMk1qIiwKICJleHAiOiAxMzExMjgxOTcwLAogImlhdCI6IDEzMTEyODA5Nz
+    AKfQ.ggW8hZ1EuVLuxNuuIJKX_V8a_OMXzR0EHR9R6jgdqrOOF4daGU96Sr_P6q
+    Jp6IcmD3HP99Obi1PRs-cwh3LO-p146waJ8IhehcwL7F09JdijmBqkvPeB2T9CJ
+    NqeGpe-gccMg4vfKjkM8FcGvnzZUN4_KSP0aAp1tOJ1zZwgjxqGByKHiOtX7Tpd
+    QyHE5lcMiKPXfEIQILVq0pc_E2DzL7emopWoaoZTF_m0_N0YzFC6g6EJbOEoRoS
+    K5hoDalrcvRYLSrQAZZKflyuVCyixEoV9GfNQC3_osjzw2PAithfubEEBLuVVk4
+    XUVrWOLrLl0nx7RkKU8NXNHq-rvKMzqg'''
+    }
--- tests/test_oidc_provider.py
+++ tests/test_oidc_provider.py
@@ -1,8 +1,10 @@
+import json
+from urllib.parse import urlencode
 from flask import url_for, request
 import mock
 import pytest
 
-from oic.oic import AuthorizationRequest
+from oic.oic import AuthorizationRequest, AccessTokenResponse
 
 
 class TestOIDCProvider(object):
@@ -33,9 +35,25 @@ class TestOIDCProvider(object):
         assert resp.status_code == 302
         assert resp.location.endswith(url_for('main.authentication_request'))
 
-    @pytest.mark.xfail
-    def test_handle_token_request(self):
-        assert False
+    @mock.patch('pyop.provider.Provider.handle_token_request')
+    def test_handle_token_request(self, mock_handle_token_request, app_,
+                                  client, request_params, request_headers,
+                                  token_response):
+
+        mock_handle_token_request.return_value = \
+            AccessTokenResponse().deserialize(urlencode(token_response))
+
+        resp = client.post(url_for('oidc_provider.token'),
+                           data=urlencode(request_params),
+                           headers=request_headers)
+
+        mock_handle_token_request.assert_called_once_with(
+            urlencode(request_params),
+            request.headers)
+
+        assert resp.status_code == 200
+        assert resp.mimetype == 'application/json'
+        assert json.loads(resp.get_data(as_text=True)) == token_response
 
     @pytest.mark.xfail
     def test_handle_userinfo_request(self):
