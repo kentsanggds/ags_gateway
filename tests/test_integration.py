0ffc833e2b53b21fb12d50996f4c1677898eac42
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-11 16:18:04 +0000

    Test user flow from email to confirm department

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -8,3 +8,20 @@ class TestIntegration(object):
 
         assert ('GOV.UK - The best place to find government services and '
                 'information') in browser.title
+
+        assert browser.is_text_present('Do you know your work email?')
+
+    def test_get_ags_gateway_clicked(self, live_server, browser):
+        browser.visit(url_for('main.request_email_address', _external=True))
+
+        browser.find_by_text('Yes').click()
+
+        browser.fill('email_address',
+                     'some.one@digital.cabinet-office.gov.uk')
+
+        browser.find_by_text('Continue').click()
+
+        assert browser.is_text_present(
+            'Email address you provided')
+        assert browser.is_text_present(
+            'some.one@digital.cabinet-office.gov.uk')
