965d0f8f74d4da08596ebc324eee3f8a381080d7
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-13 11:57:00 +0000

    Cleaner PaaS deploy script

--- paas-deploy.sh
+++ paas-deploy.sh
@@ -11,12 +11,18 @@ cf api https://api.cloud.service.gov.uk
 cf auth "$CF_USER" "$CF_PASSWORD"
 
 cf target -o csd-sso -s sandbox
-cf push $APPNAME
-cf set-env $APPNAME OIDC_CLIENT_ID "$OIDC_CLIENT_ID"
-cf set-env $APPNAME OIDC_CLIENT_SECRET "$OIDC_CLIENT_SECRET"
-cf set-env $APPNAME OIDC_CLIENT_ISSUER "$OIDC_CLIENT_ISSUER"
-cf set-env $APPNAME SERVER_NAME "$SERVER_NAME"
-cf restage $APPNAME
+
+echo '  env:' >> manifest.yml
+echo '      OIDC_CLIENT_ID: '$OIDC_CLIENT_ID >> manifest.yml
+echo '      OIDC_CLIENT_SECRET: '$OIDC_CLIENT_SECRET >> manifest.yml
+echo '      OIDC_CLIENT_ISSUER: '$OIDC_CLIENT_ISSUER >> manifest.yml
+echo '      SERVER_NAME: '$SERVER_NAME >> manifest.yml
+
+cf push $CF_APPNAME
+
+# remove the env block in case it gets pushed again using this script 
+# without destroying the app
+head -n 4 manifest.yml > temp.yml ; mv temp.yml manifest.yml
 
 # Destroy token
 cf logout
