c86dde13f14d9bd42174438ee72eceef89155d2a
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-17 10:36:21 +0000

    Updated test

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -16,13 +16,12 @@ class TestIntegration(object):
             self, live_server, browser):
         browser.visit(url_for('main.request_email_address', _external=True))
 
-        browser.find_by_text('Yes').click()
-
+        browser.find_by_css('#email_known-0').first.click()
         browser.fill('email_address',
                      'some.one@digital.cabinet-office.gov.uk')
 
-        browser.find_by_text('Continue').click()
-
+        browser.find_by_xpath(
+            '//*[@id="content"]/form/div[3]/button').first.click()
         assert browser.is_text_present(
             'Email address you provided')
         assert browser.is_text_present(
