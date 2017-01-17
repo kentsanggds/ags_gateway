cd1573ace3f5a53cb0cef6a07201066093c82bd4
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-17 19:09:43 +0000

    Add selector on confirm email page

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,5 +1,6 @@
-
 from flask import url_for
+
+from selenium.webdriver.common.keys import Keys
 
 
 class TestIntegration(object):
@@ -16,12 +17,16 @@ class TestIntegration(object):
             self, live_server, browser):
         browser.visit(url_for('main.request_email_address', _external=True))
 
-        browser.find_by_css('#email_known-0').first.click()
+        browser.choose('email_known', 'yes')
+
+        browser.is_element_present_by_css(
+            "#email_address", wait_time=0.1)
+
         browser.fill('email_address',
                      'some.one@digital.cabinet-office.gov.uk')
 
-        browser.find_by_xpath(
-            '//*[@id="content"]/form/div[3]/button').first.click()
+        browser.driver.find_element_by_xpath(
+            '//*[@id="content"]/form/div[3]/button').send_keys(Keys.ENTER)
         assert browser.is_text_present(
             'Email address you provided')
         assert browser.is_text_present(
