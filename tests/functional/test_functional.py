38c2e6d9d6e4f5f7d8e3ce244e73fd7b18b9f26d
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-18 12:50:42 +0000

    Tidied up test

--- tests/functional/test_functional.py
+++ tests/functional/test_functional.py
@@ -1,7 +1,5 @@
 from flask import url_for
 
-from selenium.webdriver.common.keys import Keys
-
 
 class TestFunctional(object):
 
@@ -19,14 +17,11 @@ class TestFunctional(object):
 
         browser.choose('email_known', 'yes')
 
-        browser.is_element_present_by_css(
-            "#email_address", wait_time=0.1)
-
         browser.fill('email_address',
                      'some.one@digital.cabinet-office.gov.uk')
 
-        browser.driver.find_element_by_xpath(
-            '//*[@id="content"]/form/div[3]/button').send_keys(Keys.ENTER)
+        browser.find_by_xpath(
+            '//*[@id="content"]/form/div[3]/button').click()
         assert browser.is_text_present(
             'Email address you provided')
         assert browser.is_text_present(
