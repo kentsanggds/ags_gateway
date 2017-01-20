044b7c21a8ab62b326e66a2334d0f496a45aebf1
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-20 10:15:35 +0000

    Tests confirm email functionality on first visit

--- tests/functional/test_high_level_user_flows.py
+++ tests/functional/test_high_level_user_flows.py
@@ -21,3 +21,27 @@ class When_on_confirm_email_with_yes_selected_and_valid_email(object):
             '#confirm-dept > div > div').value == email_address
         assert department in browser.find_by_css(
             '#confirm-dept > h2').value
+
+
+class When_first_visiting_confirm_email(object):
+
+    def it_does_not_show_email_textbox(self, live_server, browser):
+        browser.visit(url_for('main.request_email_address', _external=True))
+
+        assert not browser.find_by_name('email_address').visible
+
+    def it_does_not_select_any_radio_buttons(self, live_server, browser):
+        browser.visit(url_for('main.request_email_address', _external=True))
+
+        assert not browser.find_by_css('#email_known-0').checked
+        assert not browser.find_by_css('#email_known-1').checked
+
+    def it_does_not_go_to_department_confirm_when_continue_clicked(
+            self, live_server, browser):
+
+        browser.visit(url_for('main.request_email_address', _external=True))
+
+        browser.find_by_css('form button').click()
+
+        assert browser.url == url_for(
+            'main.request_email_address', _external=True)
