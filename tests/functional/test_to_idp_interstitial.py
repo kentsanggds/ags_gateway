de5689ee9df3af0db38cbf3cff3c2add6523e19c
Ken Tsang <ken.tsang@digital.cabinet-office.gov.uk>
2017-01-27 17:53:19 +0000

    Tests added

--- tests/functional/test_to_idp_interstitial.py
+++ tests/functional/test_to_idp_interstitial.py
@@ -1,5 +1,7 @@
-import pytest
+import time
+
 from flask import url_for
+import pytest
 
 
 @pytest.mark.usefixtures('live_server')
@@ -8,17 +10,29 @@ class When_on_to_idp_interstitial(object):
     @pytest.fixture(autouse=True)
     def setup_page(
             self,
-            browser,
             email_address,
             submit_known_email_address,
             click_continue_button):
         submit_known_email_address(email_address)
         click_continue_button()
 
-    @pytest.mark.xfail
-    def it_goes_to_the_idp_after_waiting_for_countdown(self, browser, config):
-        assert False
+    def it_goes_to_the_idp_after_clicking_continue(
+            self, browser, config, responses):
 
-    @pytest.mark.xfail
-    def it_goes_to_the_idp_after_clicking_continue(self, browser):
-        assert False
+        browser.find_by_css(
+            '#content>div.pagination>ul>'
+            'li>a>span.pagination-text>span').click()
+
+        assert browser.url.startswith(config.get('OIDC_CLIENT')['issuer'])
+
+    def it_goes_to_the_idp_after_waiting_for_countdown(self, browser, config):
+        counter = 0
+        while browser.url == url_for('main.to_idp', _external=True):
+            time.sleep(1)
+            counter += 1
+
+            # to prevent an endless loop
+            if counter > config.get('META_REFRESH_DELAY'):
+                break
+
+        assert browser.url.startswith(config.get('OIDC_CLIENT')['issuer'])
